e7bad3cf9c9a9921c9458a21e1b68751adcba6c1
santhusanty2026 <santhusanty2026@gmail.com>
2026-01-14 23:40:11 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 5
+num2 = 7
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the result using an f-string (formatted string literal)
+print(f"The sum of {num1} and {num2} is: {result}")
